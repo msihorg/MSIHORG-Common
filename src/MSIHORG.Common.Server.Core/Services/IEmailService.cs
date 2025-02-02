ab7c8d0746267951ccb2e88092e56a39380a9503
Barry Schneider (MSIH) <bschneider@msih.com>
2025-02-02 11:49:24 -0500

    interface and dto

---
+++ src/MSIHORG.Common.Server.Core/Services/IEmailService.cs
@@ -0,0 +1,19 @@
+﻿using MSIHORG.Common.Shared.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MSIHORG.Common.Server.Core.Services
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(EmailMessage emailMessage);
+        Task SendTemplatedEmailAsync(string to, string templateId, object templateData);
+        Task SendBulkEmailAsync(IEnumerable<string> to, string subject, string body);
+        Task SendEmailWithAttachmentAsync(string to, string subject, string body,
+            IEnumerable<EmailAttachment> attachments);
+    }
+}
