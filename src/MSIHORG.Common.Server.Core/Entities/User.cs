f6c70a092a74c2f2c9621ab2e8e8d6ac9cf771ac
Barry Schneider (MSIH) <bschneider@msih.com>
2025-02-07 17:59:54 -0500

    added role entity

---
+++ src/MSIHORG.Common.Server.Core/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSIHORG.Common.Server.Core.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? LastModifiedAt { get; set; }
+        public bool IsEmailConfirmed { get; set; }
+        public bool IsPhoneConfirmed { get; set; }
+        public List<string> Roles { get; set; } = new();
+    }
+}
